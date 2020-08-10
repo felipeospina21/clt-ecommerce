9b9180e1098e33b9d6199a8370a0a9f492a54cf6
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-08-10 17:03:56 -0500

    Header component and collection items added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE</h1>
-	</div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
 	return (
-		<Switch>
-			<div>
+		<div>
+			<Header />
+			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route path='/hats' component={HatsPage} />
-			</div>
-		</Switch>
+				<Route path='/shop' component={ShopPage} />
+			</Switch>
+		</div>
 	);
 }
 

4b0a4acf7190384dd4bd98cd8a04d321d2b57adf
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-08-13 18:34:33 -0500

    finishing signin and signup methods

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,7 +16,7 @@ const Header = ({ currentUser }) => (
 			<Link className='option' to='/shop'>
 				CONTACT
 			</Link>
-			{currentUser ? (
+			{currentUser? (
 				<div className='option' onClick={() => auth.signOut()}>
 					SIGN OUT
 				</div>

670c248d673e156764d9f8aabcafe0cf2c9f6eb6
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-08-14 18:28:25 -0500

    redux install and initial implementation

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,17 @@
+const INTITIAL_STATE = {
+	currentUsesr : null
+};
+
+const userReducer = (state = INTIAL_STATE, action) => {
+	switch (action.type) {
+		case 'SET_CURRENT_USER':
+			return {
+				...state,
+				currentUser : action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+export default userReducer;
